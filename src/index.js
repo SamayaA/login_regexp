1864c88f159eef5ff2f3bb633904ddd079d82676
SamayaA <asadullaevasamaya@gmail.com>
2022-09-05 23:51:04 +0300

    Fixed eslint probl

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default class Validator {
   validateUsername(login) {
-    this.login = login
+    this.login = login;
     const regexp = /^[A-Za-z]([A-Za-z]*((?<![_-])[_-][A-Za-z]*){0,}(?<!\d\d)\d[A-Za-z]*){0,}[A-Za-z]$/g;
     return regexp.test(this.login);
   }
