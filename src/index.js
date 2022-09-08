ad47d110a3e4971876649aad1514ec0430b82fcf
SamayaA <asadullaevasamaya@gmail.com>
2022-09-08 19:11:47 +0300

    Combined 2 regexp

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ export default class Validator {
     this.login = login;
     // starts and end with letter
     const regexp1 = /^[a-zA-Z][a-zA-Z0-9\-_]*[a-zA-Z]$/;
-    // 3 digits in a row
-    const regexp2 = /(\d\d\d)/;
-    // have two -_ in a row
-    const regexp3 = /([-_]+[-_]+)/;
+    // 3 digits in a row or have two -_ in a row
+    const regexp2 = /((\d\d\d)|([-_]+[-_]+))/;
     // have only one letter
-    const regexp4 = /^[a-zA-Z]$/;
-    const result = (regexp1.test(this.login)
-      && !(regexp2.test(this.login)) && !(regexp3.test(this.login)))
-      || regexp4.test(this.login);
+    const regexp3 = /^[a-zA-Z]$/;
+    const result = ((regexp1.test(this.login)
+      && !(regexp2.test(this.login)))
+      || (regexp3.test(this.login)));
     return result;
   }
 }
